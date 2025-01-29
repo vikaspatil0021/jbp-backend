f45f154f0b763dba2ffcd032def3f23e66869da3
Vikas Patil <vikaspatil0021o+github@gmail.com>
2025-01-29 21:47:55 +0530

    cors error

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(cookieParser());
 app.use(cors({
     credentials: true,
     optionSuccessStatus: 200,
-    origin: ["https://job-posting-board-liart.vercel.app", "http://localhost:3000"]
+    origin: ["https://job-posting-board-liart.vercel.app", "https://job-posting-board.skywaveapp.site","http://localhost:3000"]
 }));
 app.use(router);
 
